1c38e9bf19c92ff61f6838fea0118907b159170d
xavitech29 <sxavito01@gmail.com>
2024-02-08 20:56:38 -0500

    Agregar archivos de proyecto.

---
+++ UI.Windows/Formularios/FrmPrincipal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Windows.Formularios
+{
+    public partial class FrmPrincipal : Form
+    {
+        private int childFormNumber = 0;
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void ToolBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStrip.Visible = toolBarToolStripMenuItem.Checked;
+        }
+
+        private void StatusBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            statusStrip.Visible = statusBarToolStripMenuItem.Checked;
+        }
+
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+
+        private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Form childForm in MdiChildren)
+            {
+                childForm.Close();
+            }
+        }
+
+        // Se debe crear primero presionar en el ToopStrip del MDI y cambiar por los datos correctos
+
+         /* private void estudianteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CrearEstudiante();
+        }
+
+        private void CrearEstudiante()
+        {
+            FrmEstudiante nuevo = new FrmEstudiante();
+            nuevo.MdiParent = this;
+            nuevo.Show();
+        }*/
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
